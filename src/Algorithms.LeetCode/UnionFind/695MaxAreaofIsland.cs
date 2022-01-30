1c9bc5fde5f8f2a194ce1378508afebf85beb5a2
Artsiom Tserashkovich <artsiomtserashkovich@tserashkovich.engineer>
2022-01-30 12:47:06 +0300

    30.01

---
+++ src/Algorithms.LeetCode/UnionFind/695MaxAreaofIsland.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Algorithms.LeetCode.UnionFind
+{
+    // TimeComplexity = O(n*m) -> n = rowSize; m = colSize;
+    // MemoryComplexity = O(n*m) -> n = rowSize; m = colSize;
+    public class MaxAreaOfIsland 
+    {
+        public int GetMaxAreaOfIsland(int[][] grid)
+        {
+            var maxArea = 0;
+            var visited = new bool[grid.Length, grid[0].Length];
+            
+            for (int row = 0; row < grid.Length; row++)
+            {
+                for (int col = 0; col < grid[0].Length; col ++)
+                {
+                    if (grid[row][col] == 1 && !visited[row, col])
+                    {
+                        maxArea = Math.Max(maxArea, GetIslandAreaDFS(grid, row, col, visited));
+                    }
+                }
+            }
+
+
+            return maxArea;
+        }
+
+        private static int GetIslandAreaDFS(int[][] grid, int row, int col, bool[,] visited)
+        {
+            if (visited[row, col])
+            {
+                return 0;
+            }
+            visited[row, col] = true;
+            int area = 1;
+
+            if ((row !=0) && (grid[row -1][col] == 1) && (!visited[row -1, col]))
+            {
+                area += GetIslandAreaDFS(grid, row - 1, col, visited);
+            }
+            
+            if ((col !=0) && (grid[row][col - 1] == 1) && (!visited[row, col - 1]))
+            {
+                area += GetIslandAreaDFS(grid, row, col - 1, visited);
+            }
+
+            if ((row != grid.Length - 1) && (grid[row + 1][col] == 1) && (!visited[row + 1, col]))
+            {
+                area += GetIslandAreaDFS(grid, row + 1, col, visited);
+            }
+            
+            if ((col != grid[0].Length - 1) && grid[row][col + 1] == 1 && !visited[row, col + 1])
+            {
+                area += GetIslandAreaDFS(grid, row, col + 1, visited);
+            }
+
+            return area;
+        }
+    }
+}
---
+++ tests/Algorithms.LeetCode.Tests/UnionFind/695MaxAreaofIslandTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Algorithms.LeetCode.UnionFind;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Algorithms.LeetCode.Tests.UnionFind
+{
+    public class MaxAreaofIslandTests 
+    {
+        [TestCaseSource(nameof(GetMaxAreaofIslandCases))]
+        public void Solution(int[][] grid, int result)
+        {
+            new MaxAreaOfIsland().GetMaxAreaOfIsland(grid).Should().Be(result);
+        }
+
+        public static IEnumerable<TestCaseData> GetMaxAreaofIslandCases()
+        {
+            yield return new TestCaseData(
+            new int[][]
+            {
+                        
+                    
+                new int[] {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                new int[] {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                new int[] {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                new int[] {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                new int[] {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                new int[] {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                new int[] {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                new int[] {0,0,0,0,0,0,0,1,1,0,0,0,0}
+            }, 6);
+            
+        }
+    }
+}
