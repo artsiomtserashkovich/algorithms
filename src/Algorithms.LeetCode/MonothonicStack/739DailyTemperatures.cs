14a5683e566cfaa9dbda806e73f89d0f35105cff
Artsiom Tserashkovich <artsiomtserashkovich@gmail.com>
2022-03-13 15:21:56 +0200

    Create 739DailyTemperatures.cs

---
+++ src/Algorithms.LeetCode/MonothonicStack/739DailyTemperatures.cs
@@ -0,0 +1,50 @@
+public class DailyTemperatures 
+{
+    public int[] DailyTemperaturesV3(int[] temperatures) 
+    {
+        if(temperatures.Length == 0)
+        {
+            return Array.Empty<int>();
+        }
+        var stack = new Stack<int>();
+        var result = new int[temperatures.Length];
+        
+        for(int i = 0; i < temperatures.Length; i++)
+        {
+            while(stack.Any() && temperatures[i]  > temperatures[stack.Peek()])
+            {
+                var index = stack.Pop();
+                result[index] = i - index;
+            }
+            
+            stack.Push(i);
+        }
+        
+        return result;
+    }
+    
+    public int[] DailyTemperaturesV2(int[] temperatures) 
+    {
+        if(temperatures.Length == 0)
+        {
+            return Array.Empty<int>();
+        }
+        var heap = new PriorityQueue<int, int>();
+        var result = new int[temperatures.Length];
+        
+        heap.Enqueue(0, temperatures[0]);
+        
+        for(int i = 1; i < temperatures.Length; i++)
+        {
+            while(heap.TryPeek(out var index, out var priority) && priority < temperatures[i])
+            {
+                heap.Dequeue();
+                result[index] = i - index;
+            }
+            
+            heap.Enqueue(i, temperatures[i]);
+        }
+        
+        return result;
+    }
+}
